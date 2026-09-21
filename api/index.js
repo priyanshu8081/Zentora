e7fb424e976e1804008151d1b4e77e9381d1b5ee
Priyanshu8081 <stgamerrajpoot@gmail.com>
2026-09-21 21:56:33 +0530

    fix backend

--- api/index.js
+++ api/index.js
@@ -3,15 +3,24 @@ import cors from 'cors';
 import 'dotenv/config';
 import router from './router/router.js';
 import dbconnect from './config/db.js';
-const app=express();
-const PORT= process.env?.PORT || 9000;
-app.use(cors( {origin: process.env.CLIENT_URL}))
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
+
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({
         success: true,
-        message: 'Zentora API is running'
+        message: 'API is running'
     });
 });
+
 dbconnect();
+
 app.use(router);
+
+export default app;
