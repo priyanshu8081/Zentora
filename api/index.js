684eb637e23af1dc3a74b4d42c424815e081edd7
Priyanshu8081 <stgamerrajpoot@gmail.com>
2026-09-21 17:23:14 +0530

    backend done

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 import router from './router/router.js';
 import dbconnect from './config/db.js';
 const app=express();
-const PORT=9000;
-app.use(cors())///frontend se baat krne k liye 
+const PORT= process.env?.PORT || 9000;
+app.use(cors( {origin: process.env.CLIENT_URL}))
 app.use(express.json());
 dbconnect();
 app.use(router);
